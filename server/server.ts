a92132c91a792a9d5554c38b30d779b41c793e71
Jake McInerney <jakemcinerney1@gmail.com>
2023-09-27 22:19:53 -0500

    create custom middleware

--- server/server.ts
+++ server/server.ts
@@ -1,29 +1,41 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 
 import router from './router';
 
 const app = express();
 
+// The following is a function that returns a function that returns a message and then calls the next() piece of the stack
+// This function is called by the app below
+const customLogger = (message) => (req, res, next) => {
+  console.log(`Hello from ${message}`);
+  next();
+}
+
 /**
  * Middleware
  */
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 // Custom Middleware
 app.use((req, res, next) => {
   req.shhhh_secret = 'dog';
   next();
 });
 /**
- * The following is custom middleware that stops the next() call and prevents a request to continue further along into the next routes
+ * The following is custom middleware that stops the next() call and prevents a request from continuing further
 // app.use((req, res, next) => {
-//   req.poo = 'poo';
 //   res.status(401);
 //   res.send('Nope');
 // });
  */
+
+app.use(customLogger('customLogger'));
+
 
 
 /**
