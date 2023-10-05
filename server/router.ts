daabe5e47ae1d740b9236cf28f8e49967e5f6b8e
Jake McInerney <jakemcinerney1@gmail.com>
2023-10-04 22:25:25 -0500

    add middleware module

--- server/router.ts
+++ server/router.ts
@@ -1,4 +1,7 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
+
+import { validate } from '../modules/middleware';
 
 const router = Router();
 
@@ -6,32 +9,54 @@ const router = Router();
  * Product
  */
 
-router.get('/product', (req, res) => {
-  res.json({ message: req.shhhh_secret });
-})
-router.get('/product/:id', () => {})
-router.put('/product/:id', () => {})
-router.post('/product', () => {})
-router.delete('/product/:id', () => {})
+router.get('/product', 
+  validate([
+    body('name').isString(),
+    body('belongsToId').isString()
+  ]),
+  (req, res) => {
+    res.json({ message: req.shhhh_secret });
+  }
+);
+router.get('/product/:id', 
+  validate([
+    body('name').isString()
+  ]),
+  (req, res) => {
+
+  }
+);
+router.put('/product/:id',
+  validate([
+    body('name').isString()
+  ]),
+  (req, res) => {
+    res.json({ name: req.body.name });
+  }
+);
+router.post('/product', (req, res) => {
+
+});
+router.delete('/product/:id', () => {});
 
 /** 
  * Update
  */
 
-router.get('/update', () => {})
-router.get('/update/:id', () => {})
-router.put('/update/:id', () => {})
-router.post('/update', () => {})
-router.delete('/update/:id', () => {})
+router.get('/update', () => {});
+router.get('/update/:id', () => {});
+router.put('/update/:id', () => {});
+router.post('/update', () => {});
+router.delete('/update/:id', () => {});
 
 /** 
  * Update Point
  */
 
-router.get('/updatepoint', () => {})
-router.get('/updatepoint/:id', () => {})
-router.put('/updatepoint/:id', () => {})
-router.post('/updatepoint', () => {})
-router.delete('/updatepoint/:id', () => {})
+router.get('/updatepoint', () => {});
+router.get('/updatepoint/:id', () => {});
+router.put('/updatepoint/:id', () => {});
+router.post('/updatepoint', () => {});
+router.delete('/updatepoint/:id', () => {});
 
 export default router;
